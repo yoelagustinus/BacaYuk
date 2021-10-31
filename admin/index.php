772c3af19da11e4724aa9cc4a8f82bde9e664f67
yoelagustinus <yoelagustinus7@gmail.com>
2021-10-31 23:15:14 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -14,37 +14,43 @@
 <br>
 <div class="container">
     <!-- <button type="button" class="btn btn-primary">Add New Konten</button> -->
-    <?php echo "<a href='CreateContent.php'><button type='button' class='btn btn-primary'>Add New Konten</button></a>"; ?>
+    <?php echo "<a href='CreateContent.php'><button type='button' class='btn btn-success'>Add New Konten</button></a>"; ?>
     <br><br>
     <table class="table table-striped">
         <thead>
             <tr>
-                <th scope="col">#</th>
+                <!-- <th scope="col">#</th> -->
                 <th scope="col">Judul Konten</th>
                 <th scope="col">Kategori</th>
-                <th scope="col">Tanggal</th>
+                <th scope="col">Tanggal Upload</th>
                 <th scope="col">Actions</th>
             </tr>
         </thead>
     <tbody>
-        <tr>
-            <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>actions</td>
-        </tr>
-        <!-- <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-        </tr>
-        <tr>
-            <th scope="row">3</th>
-            <td colspan="2">Larry the Bird</td>
-            <td>@twitter</td>
-        </tr> -->
+        <?php
+            
+            $cursor = $db->post->find();
+            foreach($cursor as $post){
+                $id_content = $post->_id;
+                
+        ?>
+                <tr>
+                    <!-- <th scope="row">1</th> -->
+                    <td><?php echo $post->title ?></td>
+                    <td><?php echo $post->category ?></td>
+                    <td><?php echo $post->created_at ?></td>
+                    <td>
+                        <?php echo "<a href='EditContent.php?ContentId=$id_content&'><button type='button' class='btn btn-primary'>Edit</button></a>"; ?>
+                        <br><br>
+                        <!-- <?php echo "<a href='DeleteContent.php?ContentId=$id_content&'><button type='button' class='btn btn-danger'>Delete</button></a>"; ?> -->
+                    </td>
+                </tr>
+        <?php
+            
+
+            }
+        ?>
+        
     </tbody>
     </table>
 </div>
