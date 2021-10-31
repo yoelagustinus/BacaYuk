a88649e983dc52f3398861f9c61b983019db45d8
yoelagustinus <yoelagustinus7@gmail.com>
2021-11-01 00:08:50 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,16 @@
 <?php
     require 'header.php';
     require '../config.php';
-    
+
+    if (isset($_SESSION['success'])) {
+        
+        echo '<div class="alert alert-success d-flex align-items-center" role="alert">
+            <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+            <div>
+            '.$_SESSION['success'].'
+            </div>
+        </div>';
+    }
 ?>
 
 <!DOCTYPE html>
