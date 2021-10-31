ed50ac100ef650721e9166e3315827bc317778f2
yoelagustinus <yoelagustinus7@gmail.com>
2021-10-31 22:01:07 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -1,14 +1,7 @@
 <?php
     require 'header.php';
     require '../config.php';
-    if(isset($_POST['submit'])){
-        $insertOneResult = $collection->message->insertOne([
-            
-            'pesan' => $_POST['pesan'],
-            
-        ]);
-        header("Location: index.php");
-    }
+    
 ?>
 
 <!DOCTYPE html>
