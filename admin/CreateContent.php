8eb7b4a563671f5b4ed9755c2b2ab40f3c871b3a
Yoel Agustinus <yoelagustinus7@gmail.com>
2021-11-01 19:29:31 +0700

    Update CreateContent.php

--- admin/CreateContent.php
+++ admin/CreateContent.php
@@ -20,7 +20,7 @@
             'created_at' => $today_date
      
         ]);
-        $_SESSION['success'] = "Content '/$content->title/' berhasil dibuat";
+        $_SESSION['success'] = "Content '" .  $_POST['title'] . "' berhasil dibuat";
         header("Location: index.php");
     }
 
