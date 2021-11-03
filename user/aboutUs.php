886103fef7c5d6bb5f19da805246c25fd281cf0c
Femielice <71973776+Femielice@users.noreply.github.com>
2021-11-03 22:32:30 +0700

    menambahkan tampilan baru

---
+++ user/aboutUs.php
@@ -0,0 +1,29 @@
+<html>
+    <body>
+        <?php include 'headerUser.php';?>
+        <div class="container p-3">
+            <img src="../images_thumb/membaca.jpg" width="500" class="img-thumbnail mb-3" alt="...">
+            <h3 class="mb-1">Baca Yuk</h3>
+            <p>Membaca merupakan kegiatan melihat tulisan bacaan dan proses memahami isi teks dengan bersuara atau dalam hati. 
+                Membaca adalah mengungkapkan suatu imajinasi terhadap suatu pembaca yang disukai khalayak ramai dan juga dimengerti 
+                oleh seseorang yang dicintai. Membaca menjadi salah satu jenis kemampuan berbahasa melalui tulisan yang bersifat reseptif 
+                karena dengan membaca seseorang akan memperoleh informasi, ilmu pengetahuan, dan pengalaman-pengalaman baru yang belum pernah 
+                diketahui sebelumnya. Kegiatan membaca bukan sekedar aktivitas yang bersifat pasif dan respektif saja, melainkan menghendaki 
+                pembaca untuk aktif berpikir ketika sedang melihat kata-kata yang terdapat di dalam buku. 
+            </p>
+            <p>Untuk mewujudkan hal-hal yang ada disini maka kami membangun sebuah website untuk membaca secara online agar para user memiliki 
+                kemudahan dalam membaca dimana saja. Disini kami menggunakan database mongoDB, mongoDB adalah salah satu jenis database NoSQL 
+                yang cukup populer digunakan dalam pengembangan website. Berbeda dengan database jenis SQL yang menyimpan data menggunakan relasi tabel, 
+                MongoDB menggunakan dokumen dengan format JSON. 
+            </p>
+            <p> Kelompok kami beranggota 4 orang yaitu:
+                <ul>
+                    <li>Johanes Victor</li>
+                    <li>Femi Elice</li>
+                    <li>Reynold Yehezkiel</li>
+                    <li>Yoel Agustinus</li>
+                </ul>
+            </p>    
+        </div>
+    </body>
+</html>
---
+++ user/kesehatan.php
@@ -0,0 +1,54 @@
+<html>
+    <body>
+        <!-- Tampilan -->
+        <div class="tampilan">
+
+            <!-- navbar -->
+            <?php include 'headerUser.php';?>
+
+            <!-- konten -->
+            <div class="container p-4">
+                <div class="d-flex justify-content-center mb-2">
+                    <div class="mx-auto mb-4" style="width: 650px;">
+                        <div class="card" style="width: 40rem;">
+                            <img src="../images_thumb/kesehatan.jpg" class="card-img-top" alt="...">
+                        </div>
+                    </div>
+                </div>
+                <h5>Kategori: Kesehatan</h5>
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Kenali Gejala HIV pada Anak</h5>
+                                <p>Ada beberapa gejala dan tanda HIV pada anak yang sudah mulai terlihat sejak tahun pertama kehidupannya. Ini meliputi gejala awal yang ringan hingga gejala infeksi parah yang sering kambuh. 
+                                    Gejala ini patut diantisipasi jika anak terlahir dari orang tua yang memiliki infeksi HIV dan tidak mendapatkan pengobatan. <a href="artikel.php" class="text-decoration-none"> Baca selengkapnya</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Cara Mengatasi Alergi Susu Sapi Pada Anak</h5>
+                                <p>Alergi susu sapi merupakan alergi makanan yang paling umum terjadi pada anak-anak, terutama yang usianya masih di bawah 3 tahun. Masalah ini tidak boleh disepelekan dan perlu ditangani dengan baik, 
+                                    karena anak membutuhkan nutrisi dari susu agar tumbuh kembangnya optimal.<a href="#" class="text-decoration-none"> Baca selengkapnya</a>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Vaksin Sinovac untuk Anak Usia 6-11 Tahun, Ini Pesan Ketua IDAI Bagi Orangtua</h5>
+                                <p>Ketua Ikatan Dokter Anak Indonesia (IDAI) menyambut baik kabar vaksin Sinovac kini sudah mendapat izin dari Badan Pengawas Obat dan Makanan (BPOM) untuk diberikan kepada anak usia 6-11 tahun. 
+                                    Sepertu diberitakan sebelumnya, BPOM telah menyetujui penggunaan vaksin Sinovac, <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
+                                </p>   
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
