766de684d03ad7f54e5790cdd9793966809a3062
yoelagustinus <yoelagustinus7@gmail.com>
2021-11-04 00:00:35 +0700

    Update home.php

--- user/home.php
+++ user/home.php
@@ -4,7 +4,13 @@
         <div class="tampilan">
 
             <!-- navbar -->
-            <?php include 'headerUser.php';?>
+            <?php 
+                include 'headerUser.php';
+
+                require '../config.php';
+                require 'ControllerVisitor.php';
+                $cursor = $db->post->find();
+            ?>
 
             <!-- konten -->
             <div class="container p-4">
@@ -19,67 +25,78 @@
                     </div>
                 </div>
                 <div class="row row-cols-1 row-cols-md-3 g-4">
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Kenali Gejala HIV pada Anak</h5>
-                                <p>Ada beberapa gejala dan tanda HIV pada anak yang sudah mulai terlihat sejak tahun pertama kehidupannya. Ini meliputi gejala awal yang ringan hingga gejala infeksi parah yang sering kambuh. 
-                                    Gejala ini patut diantisipasi jika anak terlahir dari orang tua yang memiliki infeksi HIV dan tidak mendapatkan pengobatan. <a href="artikel.php" class="text-decoration-none"> Baca selengkapnya</a>
-                                </p>
+                    <?php
+                        foreach($cursor as $post){
+                            $id_content = $post->_id;
+                    
+                    ?>
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title"><?php echo $post->title ?></h5>
+                                    <p>
+                                        <?php 
+                                    echo cutString($post->konten);
+                                    echo "<a href='Content.php?ContentId=$id_content&' class='text-decoration-none'> Baca selengkapnya</a>";
+                                    ?>
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Cara Mengatasi Alergi Susu Sapi Pada Anak</h5>
-                                <p>Alergi susu sapi merupakan alergi makanan yang paling umum terjadi pada anak-anak, terutama yang usianya masih di bawah 3 tahun. Masalah ini tidak boleh disepelekan dan perlu ditangani dengan baik, 
-                                    karena anak membutuhkan nutrisi dari susu agar tumbuh kembangnya optimal.<a href="#" class="text-decoration-none"> Baca selengkapnya</a>
-                                </p>
+                    <?php
+                        }
+                    ?>
+                        <!-- <div class="col">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Cara Mengatasi Alergi Susu Sapi Pada Anak</h5>
+                                    <p>Alergi susu sapi merupakan alergi makanan yang paling umum terjadi pada anak-anak, terutama yang usianya masih di bawah 3 tahun. Masalah ini tidak boleh disepelekan dan perlu ditangani dengan baik, 
+                                        karena anak membutuhkan nutrisi dari susu agar tumbuh kembangnya optimal.<a href="#" class="text-decoration-none"> Baca selengkapnya</a>
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Gelar Simposium, Australia-Indonesia Bertukar Pengetahuan Tangani Bencana dan Pandemi</h5>
-                                <p>Deputy Head of Mission Kedutaan Australia di Jakarta, Stephen Scott mengatakan, kawasan Indo-Pasifik memiliki pengalaman yang sangat kaya di bidang penanganan bencana dan kedaruratan kesehatan masyarakat. 
-                                    Seperti yang telah ditunjukkan <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
-                                </p>
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Gelar Simposium, Australia-Indonesia Bertukar Pengetahuan Tangani Bencana dan Pandemi</h5>
+                                    <p>Deputy Head of Mission Kedutaan Australia di Jakarta, Stephen Scott mengatakan, kawasan Indo-Pasifik memiliki pengalaman yang sangat kaya di bidang penanganan bencana dan kedaruratan kesehatan masyarakat. 
+                                        Seperti yang telah ditunjukkan <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Syarat-syarat sebagai Ilmu Pengetahuan</h5>
-                                <p>Ilmu pengetahuan adalah seperangkat pengetahuan dan informasi yang telah dikaji manusia dalam berbagai bidang. Suatu pengetahuan harus memenuhi syarat-syarat untuk menjadi ilmu pengetahuan. 
-                                    Poedjawijatna dalam buku Tahu dan Pengetahuan (2004) menerangkan bahwa syarat-syarat sebagai ilmu pengetahuan harus memenuhi <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
-                                </p>
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Syarat-syarat sebagai Ilmu Pengetahuan</h5>
+                                    <p>Ilmu pengetahuan adalah seperangkat pengetahuan dan informasi yang telah dikaji manusia dalam berbagai bidang. Suatu pengetahuan harus memenuhi syarat-syarat untuk menjadi ilmu pengetahuan. 
+                                        Poedjawijatna dalam buku Tahu dan Pengetahuan (2004) menerangkan bahwa syarat-syarat sebagai ilmu pengetahuan harus memenuhi <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
+                                    </p>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Vaksin Sinovac untuk Anak Usia 6-11 Tahun, Ini Pesan Ketua IDAI Bagi Orangtua</h5>
-                                <p>Ketua Ikatan Dokter Anak Indonesia (IDAI) menyambut baik kabar vaksin Sinovac kini sudah mendapat izin dari Badan Pengawas Obat dan Makanan (BPOM) untuk diberikan kepada anak usia 6-11 tahun. 
-                                    Sepertu diberitakan sebelumnya, BPOM telah menyetujui penggunaan vaksin Sinovac, <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
-                                </p>   
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Vaksin Sinovac untuk Anak Usia 6-11 Tahun, Ini Pesan Ketua IDAI Bagi Orangtua</h5>
+                                    <p>Ketua Ikatan Dokter Anak Indonesia (IDAI) menyambut baik kabar vaksin Sinovac kini sudah mendapat izin dari Badan Pengawas Obat dan Makanan (BPOM) untuk diberikan kepada anak usia 6-11 tahun. 
+                                        Sepertu diberitakan sebelumnya, BPOM telah menyetujui penggunaan vaksin Sinovac, <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
+                                    </p>   
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Faktor Penyebab Lemah nya Mata Uang Rupiah</h5>
-                                <p>Nilai tukar mata uang rupiah terhadap dolar, selalu mengalami perubahan setiap saat terkadang melemah terkadang juga dapat menguat.
-                                    Kenapa bisa lemah dan kuat tentu saja keduanya ada sebab nya, namun kali ini kita akan bahas penyebab melemah nya mata uang rupiah.  
-                                    Semakin lemah nya mata uang rupiah indonesia ini menyebabkan harga-harga <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
-                                </p>   
+                        <div class="col">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h5 class="card-title">Faktor Penyebab Lemah nya Mata Uang Rupiah</h5>
+                                    <p>Nilai tukar mata uang rupiah terhadap dolar, selalu mengalami perubahan setiap saat terkadang melemah terkadang juga dapat menguat.
+                                        Kenapa bisa lemah dan kuat tentu saja keduanya ada sebab nya, namun kali ini kita akan bahas penyebab melemah nya mata uang rupiah.  
+                                        Semakin lemah nya mata uang rupiah indonesia ini menyebabkan harga-harga <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
+                                    </p>   
+                                </div>
                             </div>
-                        </div>
-                    </div>
+                        </div> -->
                 </div>
             </div>
         </div>
