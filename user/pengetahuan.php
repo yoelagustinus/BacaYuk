1b9395c27d441ce93fa79bc7a61f852e00d0e19c
Femielice <71973776+Femielice@users.noreply.github.com>
2021-11-04 19:39:23 +0700

    perubahan userIU

--- user/pengetahuan.php
+++ user/pengetahuan.php
@@ -1,51 +1,47 @@
 <html>
     <body>
-        <!-- Tampilan -->
-        <div class="tampilan">
+        <!-- navbar -->
+        <?php include 'headerUser.php';?>
 
-            <!-- navbar -->
-            <?php include 'headerUser.php';?>
-
-            <!-- konten -->
-            <div class="container p-4">
-                <div class="d-flex justify-content-center mb-2">
-                    <div class="mx-auto mb-4" style="width: 650px;">
-                        <div class="card" style="width: 40rem;">
-                            <img src="../images_thumb/pengetahuan.jpg" class="card-img-top" alt="...">
+        <!-- konten -->
+        <div class="container p-4">
+            <div class="d-flex justify-content-center mb-2">
+                <div class="mx-auto mb-4" style="width: 650px;">
+                    <div class="card" style="width: 40rem;">
+                        <img src="../images_thumb/pengetahuan.jpg" class="card-img-top" alt="...">
+                    </div>
+                </div>
+            </div>
+            <h5>Kategori: Pengetahuan</h5>
+            <div class="row row-cols-1 row-cols-md-3 g-4">
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Gelar Simposium, Australia-Indonesia Bertukar Pengetahuan Tangani Bencana dan Pandemi</h5>
+                            <p>Deputy Head of Mission Kedutaan Australia di Jakarta, Stephen Scott mengatakan, kawasan Indo-Pasifik memiliki pengalaman yang sangat kaya di bidang penanganan bencana dan kedaruratan kesehatan masyarakat. 
+                                Seperti yang telah ditunjukkan <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
+                            </p>
                         </div>
                     </div>
                 </div>
-                <h5>Kategori: Pengetahuan</h5>
-                <div class="row row-cols-1 row-cols-md-3 g-4">
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Gelar Simposium, Australia-Indonesia Bertukar Pengetahuan Tangani Bencana dan Pandemi</h5>
-                                <p>Deputy Head of Mission Kedutaan Australia di Jakarta, Stephen Scott mengatakan, kawasan Indo-Pasifik memiliki pengalaman yang sangat kaya di bidang penanganan bencana dan kedaruratan kesehatan masyarakat. 
-                                    Seperti yang telah ditunjukkan <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
-                                </p>
-                            </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Syarat-syarat sebagai Ilmu Pengetahuan</h5>
+                            <p>Ilmu pengetahuan adalah seperangkat pengetahuan dan informasi yang telah dikaji manusia dalam berbagai bidang. Suatu pengetahuan harus memenuhi syarat-syarat untuk menjadi ilmu pengetahuan. 
+                                Poedjawijatna dalam buku Tahu dan Pengetahuan (2004) menerangkan bahwa syarat-syarat sebagai ilmu pengetahuan harus memenuhi <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
+                            </p>
                         </div>
                     </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Syarat-syarat sebagai Ilmu Pengetahuan</h5>
-                                <p>Ilmu pengetahuan adalah seperangkat pengetahuan dan informasi yang telah dikaji manusia dalam berbagai bidang. Suatu pengetahuan harus memenuhi syarat-syarat untuk menjadi ilmu pengetahuan. 
-                                    Poedjawijatna dalam buku Tahu dan Pengetahuan (2004) menerangkan bahwa syarat-syarat sebagai ilmu pengetahuan harus memenuhi <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-body">
-                                <h5 class="card-title">Faktor Penyebab Lemah nya Mata Uang Rupiah</h5>
-                                <p>Nilai tukar mata uang rupiah terhadap dolar, selalu mengalami perubahan setiap saat terkadang melemah terkadang juga dapat menguat.
-                                    Kenapa bisa lemah dan kuat tentu saja keduanya ada sebab nya, namun kali ini kita akan bahas penyebab melemah nya mata uang rupiah.  
-                                    Semakin lemah nya mata uang rupiah indonesia ini menyebabkan harga-harga <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
-                                </p>   
-                            </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                        <div class="card-body">
+                            <h5 class="card-title">Faktor Penyebab Lemah nya Mata Uang Rupiah</h5>
+                            <p>Nilai tukar mata uang rupiah terhadap dolar, selalu mengalami perubahan setiap saat terkadang melemah terkadang juga dapat menguat.
+                                Kenapa bisa lemah dan kuat tentu saja keduanya ada sebab nya, namun kali ini kita akan bahas penyebab melemah nya mata uang rupiah.  
+                                Semakin lemah nya mata uang rupiah indonesia ini menyebabkan harga-harga <a href="#" class="text-decoration-none"> Baca selengkapnya</a>
+                            </p>   
                         </div>
                     </div>
                 </div>
