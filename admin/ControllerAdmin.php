f9e47411eefb567f2a1c7b9ec6648f10ebd7c8da
yoelagustinus <yoelagustinus7@gmail.com>
2021-11-06 01:28:55 +0700

    Update ControllerAdmin.php

--- admin/ControllerAdmin.php
+++ admin/ControllerAdmin.php
@@ -1,11 +1,16 @@
 <?php
-function sessionCheck(){
-    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-        header("location: ../index.php");
-        exit;
-    }
-}
+include '../config.php';
 
 function detectEnter($text){
     $find = '\n';
+}
+
+
+function deleteContent($id){
+    $content = $db->post->deleteOne([
+        '_id' => new MongoDB\BSON\ObjectID($id)
+    ]);
+
+    $_SESSION['success'] = "Content telah Berhasil dihapus";
+    header("Location: index.php");
 }
