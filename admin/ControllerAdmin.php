43d7bfbd8cd51a0786da41dba021b721a5fbfbef
yoelagustinus <yoelagustinus7@gmail.com>
2021-11-05 23:09:17 +0700

    Update ControllerAdmin.php

--- admin/ControllerAdmin.php
+++ admin/ControllerAdmin.php
@@ -1,4 +1,10 @@
 <?php
+function sessionCheck(){
+    if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+        header("location: ../index.php");
+        exit;
+    }
+}
 
 function detectEnter($text){
     $find = '\n';
