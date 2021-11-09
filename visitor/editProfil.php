5d6da02a59fb6da6d09c6b1b0f96992523fe2243
Femielice <71973776+Femielice@users.noreply.github.com>
2021-11-09 21:05:55 +0700

    memperbaiki tampilan editProfil

--- visitor/editProfil.php
+++ visitor/editProfil.php
@@ -18,29 +18,36 @@
         <form action="" method="POST">
             <div class="position-absolute top-50 start-50 translate-middle">
                 <div class="container">
-                    <div class="card" style="width: 50rem;">
-                        <div class="card-body"><h4>Edit account</h4>
-                            <div class="mb-3">
-                                <label for="exampleInputName" class="form-label">Name</label>
-                                <input type="name" name="name" class="form-control" id="exampleInputName">
+                    <div class="card-group">
+                        <div class="card">
+                            <div class="card-body"><h4>Edit account</h4>
+                                <div class="mb-3">
+                                    <label for="exampleInputName" class="form-label">Name</label>
+                                    <input type="name" name="name" class="form-control" id="exampleInputName">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputEmail" class="form-label">Email address</label>
+                                    <input type="email" name="mail" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp">
+                                </div>
                             </div>
-                            <div class="mb-3">
-                                <label for="exampleInputEmail" class="form-label">Email address</label>
-                                <input type="email" name="mail" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp">
+                        </div>
+                        <div class="card">
+                            <div class="card-body"><h4>Edit account</h4>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword" class="form-label">Password</label>
+                                    <input type="password" name="password1" class="form-control" id="exampleInputPassword">
+                                </div>
+                                <div class="mb-3">
+                                    <label for="exampleInputPassword" class="form-label">Verification Password</label>
+                                    <input type="password" name="password2" class="form-control" id="exampleInputPassword">
+                                </div>
+                                <button id="edit" name="edit" class="btn btn-primary">Simpan Perubahan</button>
+                                <a href="profil.php" name="batal" class="text-muted text-decoration-none p-3">Batal</a>
                             </div>
-                            <div class="mb-3">
-                                <label for="exampleInputPassword" class="form-label">Password</label>
-                                <input type="password" name="password1" class="form-control" id="exampleInputPassword">
-                            </div>
-                            <div class="mb-3">
-                                <label for="exampleInputPassword" class="form-label">Verification Password</label>
-                                <input type="password" name="password2" class="form-control" id="exampleInputPassword">
-                            </div>
-                            <button id="edit" name="edit" class="btn btn-primary">Simpan Perubahan</button>
-                            <a href="profil.php" name="batal" class="text-muted text-decoration-none p-3">Batal</a>
                         </div>
                     </div>
-                </div>    
+                </div>   
+                    
             </div>
         </form>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
