7ca0ca38970c1cebe961c833284e95b6cf64d826
Femielice <71973776+Femielice@users.noreply.github.com>
2021-11-09 21:27:12 +0700

    memperbaiki tampilan editProfil

---
+++ visitor/editProfil.php
@@ -0,0 +1,59 @@
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title>Baca Yuk!</title>
+        <style type="text/css">
+            body { background: #ECEFF1 !important; }
+        </style>
+    </head>
+    <body>
+        <!-- Option 1: Bootstrap Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <form action="" method="POST">
+            <div class="container">
+                <div class="card"style="width: 40rem;"><h4>Edit account</h4>
+                    <div class="card-body">
+                        <div class="card-group">
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="mb-3">
+                                        <label for="exampleInputName" class="form-label">Name</label>
+                                        <input type="name" name="name" class="form-control" id="exampleInputName">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="exampleInputEmail" class="form-label">Email address</label>
+                                        <input type="email" name="mail" class="form-control" id="exampleInputEmail" aria-describedby="emailHelp">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card">
+                                <div class="card-body">
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword" class="form-label">Password</label>
+                                        <input type="password" name="password1" class="form-control" id="exampleInputPassword">
+                                    </div>
+                                    <div class="mb-3">
+                                        <label for="exampleInputPassword" class="form-label">Verification Password</label>
+                                        <input type="password" name="password2" class="form-control" id="exampleInputPassword">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="d-flex flex-row-reverse mb-3">
+                            <button id="register" name="register" class="btn btn-primary">Change</button>
+                            <a href="profil.php" class="text-decoration-none p-3 text-muted">Back</a>
+                        </div>    
+                    </div>
+                </div>
+            </div>  
+        </form>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    </body>
+</html>
