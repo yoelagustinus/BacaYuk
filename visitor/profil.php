1ec983f8b77cb1bc7c2e7245ad358f1ceabf0ab7
Femielice <71973776+Femielice@users.noreply.github.com>
2021-11-10 21:52:07 +0700

    memperbaharui profil

--- visitor/profil.php
+++ visitor/profil.php
@@ -3,25 +3,21 @@
         <?php 
             include 'headerVisitor.php';
         ?>
-        <div class="container p-3">
+        <div class="container p-2">
             <h4 class="mb-2">Profil Anda</h4>
-            <div class="card" style="width: 18rem;">
+            <div class="card bg-success" style="width: 50rem; height: 10rem;">
+                <a href="changeEmail.php" class="text-decoration-none text-white p-2 d-flex justify-content-end">Edit Profil</a>
                 <div class="card-body">
-                    <div class="row row-cols-auto">
-                        <div class="col">
-                            <img src="../images_thumb/profil.png" width="65" height="65">
-                        </div>
-                        <div class="col mb-3">
-                            <h5 class="mb-2">yumi</h5>
-                            <p class="mb-2">yumi@gmail.com</p>
-                            <a href="changeEmail.php" class="text-decoration-none text-muted mb-2">Ubah alamat email</a>
-                        </div>
-                    </div>
-                    <center>
-                        <a href="../logout.php" role="button" class="btn btn-primary btn-sm" style="width: 10rem;">Logout</a>
-                    </center> 
+                    <img src="../images_thumb/profil.png" width="95" height="95">
                 </div>
-            </div>
+            </div>    
+            <div class="card" style="width: 50rem; height: 10rem;">
+                <div class="card-body">
+                    <h4 class="mb-2">yumi</h4>
+                    <p class="mb-4">yumi@gmail.com</p>
+                    <a href="../logout.php" role="button" class="btn btn-primary btn-sm" style="width: 10rem;">Logout</a> 
+                </div>
+            </div>    
         </div>
     </body>
 </html>
