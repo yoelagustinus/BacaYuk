e402f58bd3e56be0c8ab30863e92f00eb0012aa0
Femielice <71973776+Femielice@users.noreply.github.com>
2021-11-11 21:30:35 +0700

    memperbaharui

--- admin/header.php
+++ admin/header.php
@@ -45,25 +45,40 @@ $user = $db->users->findOne([
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
               <img src="../images/BacaYuk.png" alt="" width="40" height="40">
-              <a class="navbar-brand fw-bold text-success" href="home.php">Baca Yuk</a>
+              <a class="navbar-brand fw-bold text-success" href="index.php">Baca Yuk Admin</a>
             </li>
-            <li class="nav-item">
+            <!-- <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="index.php">Konten</a>
             </li>
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="ViewUser.php">View User</a>
-            </li>
+            </li> -->
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="EditProfile.php"><?php echo $user->name ?> </a>
+              <a class="nav-link active" aria-current="page" href="EditProfile.php"><h6 class="fw-bolder"><?php echo $user->name ?></h6></a>
             </li>
           </ul>
           <div class="d-flex flex-row-reverses">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
+              <!-- <li class="nav-item">
                 <a class="nav-link active" aria-current="page" href="../index.php">LOGOUT</a>
               </li>
-            </ul>
-          </div>
+              </ul> -->
+              <!-- </div> -->
+              <li class="nav-item dropdown">
+                <a class="nav-link text-white" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Setting
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16">
+                    <path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/>
+                    <path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/>
+                  </svg>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="index.php">Konten</a></li>
+                  <li><a class="dropdown-item" href="ViewUser.php">View User</a></li>
+                  <li><a class="dropdown-item" href="../logout.php">Logout</a></li>
+                </ul>
+              </li>
+            </ul>  
+          </div>  
         </div>
       </div>
     </nav>
